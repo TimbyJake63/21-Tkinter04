4437edee59dc30a5b87a0c8fb4ac2814106dbe6f
TimbyJake63 <jtimberlake002@student.franciscan.edu>
2024-04-11 18:00:32 -0400

    done

--- src/m1_clicks.py
+++ src/m1_clicks.py
@@ -5,7 +5,7 @@ import tkinter as tk
 # In this module, all of the _todo_ items will be in one comment because you
 # will be modifying the same block of code as you go.
 #
-# TODO: 1. (1 pt)
+# DONE: 1. (1 pt)
 #
 #   First, create a tkinter window called window.
 #
@@ -15,7 +15,7 @@ import tkinter as tk
 #   Once you have done this, then change the above _TODO_ to DONE.
 #
 #
-# TODO: 2. (4 pts)
+# DONE: 2. (4 pts)
 #
 #   Now, create a frame called frm_a that has a width of 100 and height of 100.
 #
@@ -36,3 +36,18 @@ import tkinter as tk
 #   Once you have done this, then change the above _TODO_ to DONE.
 #
 ###############################################################################
+window=tk.Tk()
+def handle_click(event):
+    print(event)
+    if event.num == 1:
+        print("Left")
+    elif event.num == 2:
+        print("strangely right with a macbook or if you scroll wheel click")
+    elif event.num == 3:
+        print("Right")
+frm_a = tk.Frame(master=window, width=100, height=100)
+
+frm_a.bind("<Button>", handle_click)
+
+frm_a.pack()
+window.mainloop()
--- src/m3_button_commands.py
+++ src/m3_button_commands.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 
 ###############################################################################
-# TODO: 1. (2 pts)
+# DONE: 1. (2 pts)
 #
 #   For this _todo_, copy your code from your fillable form from Session 20 and
 #   paste it below.
@@ -13,7 +13,7 @@ import tkinter as tk
 ###############################################################################
 
 ###############################################################################
-# TODO: 2. (1 pt)
+# DONE: 2. (1 pt)
 #
 #   Now, use the command keyword to connect your "Submit" button to your
 #   print_form() function.
@@ -24,3 +24,77 @@ import tkinter as tk
 #
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
+window = tk.Tk()
+window.title("Form")
+
+def print_form():
+    name = name_entry.get()
+    address1 = address1_entry.get()
+    address2 = address2_entry.get()
+    city = city_entry.get()
+    state = state_entry.get()
+    zip_code = zip_entry.get()
+    phone = phone_entry.get()
+    email = email_entry.get()
+    
+    print("Name:", name)
+    print("Address Line 1:", address1)
+    print("Address Line 2:", address2)
+    print("City:", city)
+    print("State:", state)
+    print("Zip Code:", zip_code)
+    print("Phone Number:", phone)
+    print("Email Address:", email)
+
+name_label = tk.Label(window, text="Name:")
+name_label.grid(row=0, column=0, padx=10, pady=5, sticky=tk.W)
+name_entry = tk.Entry(window)
+name_entry.grid(row=0, column=1, padx=10, pady=5)
+
+address1_label = tk.Label(window, text="Address Line 1:")
+address1_label.grid(row=1, column=0, padx=10, pady=5, sticky=tk.W)
+address1_entry = tk.Entry(window)
+address1_entry.grid(row=1, column=1, padx=10, pady=5)
+
+address2_label = tk.Label(window, text="Address Line 2:")
+address2_label.grid(row=2, column=0, padx=10, pady=5, sticky=tk.W)
+address2_entry = tk.Entry(window)
+address2_entry.grid(row=2, column=1, padx=10, pady=5)
+
+city_label = tk.Label(window, text="City:")
+city_label.grid(row=3, column=0, padx=10, pady=5, sticky=tk.W)
+city_entry = tk.Entry(window)
+city_entry.grid(row=3, column=1, padx=10, pady=5)
+
+state_label = tk.Label(window, text="State:")
+state_label.grid(row=4, column=0, padx=10, pady=5, sticky=tk.W)
+state_entry = tk.Entry(window)
+state_entry.grid(row=4, column=1, padx=10, pady=5)
+
+zip_label = tk.Label(window, text="Zip Code:")
+zip_label.grid(row=5, column=0, padx=10, pady=5, sticky=tk.W)
+zip_entry = tk.Entry(window)
+zip_entry.grid(row=5, column=1, padx=10, pady=5)
+
+phone_label = tk.Label(window, text="Phone Number:")
+phone_label.grid(row=6, column=0, padx=10, pady=5, sticky=tk.W)
+phone_entry = tk.Entry(window)
+phone_entry.grid(row=6, column=1, padx=10, pady=5)
+
+email_label = tk.Label(window, text="Email Address:")
+email_label.grid(row=7, column=0, padx=10, pady=5, sticky=tk.W)
+email_entry = tk.Entry(window)
+email_entry.grid(row=7, column=1, padx=10, pady=5)
+
+submit_button = tk.Button(window, text="Submit", command=print_form)
+submit_button.grid(row=8, column=0, columnspan=2, pady=10)
+
+window.configure(background='grey')
+for widget in (name_label, address1_label, address2_label, city_label, state_label, zip_label, phone_label, email_label):
+    widget.configure(background='grey')
+for entry in (name_entry, address1_entry, address2_entry, city_entry, state_entry, zip_entry, phone_entry, email_entry):
+    entry.configure(background='white')
+
+window.mainloop()
+
+#I don't think I mentioned this in last weeks lab but this code form session 18 I used chatgpt to already do all this in the first place. I had it expalin everything line by line so I could understand it though but I had it already do the print and everything because I thought it would be cool. not sure if I needed to put this in here but just wanted to et you know.
